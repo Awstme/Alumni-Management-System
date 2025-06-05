09791a2ba542d1a86325e46ef1c7e73d6ee83240
Awstme <awstme@163.com>
2025-06-05 23:50:20 +0800

    feat(alumni): 添加校友类属性设置方法并优化性别表示
    - 在 Alumni 类中添加了多个 set 方法，用于设置校友的各项属性
    - 将 bool 类型的 sex 改为 string 类型的 gender，以更准确地表示性别信息
    - 优化了类的构造函数定义

--- alumni.h
+++ alumni.h
@@ -7,7 +7,7 @@ class Alumni
 {
 protected:
     string name;
-    bool sex;
+    string gender;
     int age;
     int graduationYear;
     string department;
@@ -17,5 +17,15 @@ protected:
     string qq;
     string email;
 public:
-    Alumni(string name,bool sex,int age,int graduationYear,string department,string className,string address,string phone,string qq,string email){}
+    Alumni(string name,bool sex,int age,int graduationYear,string department,string className,string address,string phone,string qq,string email){};
+    void setName(string newName){};
+    void setGender(string newGender){};
+    void setAge(int newAge){};
+    void setGraduationYear(int newGraduationYear){};
+    void setDepartment(string newDepartment){};
+    void setClassName(string newClassName){};
+    void setAddress(string newAddress){};
+    void setPhone(string newPhone){};
+    void setQq(string newQq){};
+    void setEmail(string newEmail){};
 };
